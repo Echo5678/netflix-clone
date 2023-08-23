655a8b0f2594e50657f26c081755df8b8803cddb
Echo <vincentgarciajr2712@gmail.com>
2023-08-23 12:46:59 -0700

    Added auth to db connection

--- lib/mongodb-promise.ts
+++ lib/mongodb-promise.ts
@@ -1,13 +1,14 @@
 import { MongoClientOptions } from "mongodb";
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb";
-
-// [CANNOT WORK] import "../server/models/demoModel";
 
 const uri = process.env.MONGODB_URI!;
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
+  auth: {
+    username: process.env.MONGODB_USER,
+    password: process.env.MONGODB_PASS,
+  },
 } as MongoClientOptions;
 
 let client;
@@ -18,10 +19,6 @@ if (!process.env.MONGODB_URI) {
 }
 
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-
-  // todo global._mongoClientPromise
   //@ts-ignore
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
@@ -31,11 +28,8 @@ if (process.env.NODE_ENV === "development") {
   //@ts-ignore
   clientPromise = global._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
 export default clientPromise as Promise<MongoClient>;
